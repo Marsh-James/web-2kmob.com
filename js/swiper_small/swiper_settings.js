fddd0edefc502a255cec419a41ae802ef6f72c56
unknown <jamesfkmarsh@gmail.com>
2018-04-12 01:23:50 +0100

    developed page landing

---
+++ js/swiper_small/swiper_settings.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.swiper-container-small', {
+    spaceBetween: 0,
+    centeredSlides: true,
+    
+	autoplay: {
+        delay: 12000,
+        disableOnInteraction: false,
+    },
+});
